1b58f191fb6b8467c1673c6976f46f128118087c
Lokesh <rocklinglokesh@gmail.com>
2021-07-10 01:12:32 +0530

    app completed

--- Quizapp/app/src/main/java/com/example/quizapp/MainActivity.java
+++ Quizapp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.quizapp;
 
 import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 import java.util.ArrayList;
 import java.util.Random;
+//import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public Button option1Btn, option2Btn, option3Btn,option4Btn;
     private ArrayList<QuizModel> quizModelArrayList;
     Random random;
-    int currentScore =0;
-    int questionsAttempted = 1, currentPos;
+    int currentScore =0, questionsAttempted = 1, currentPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,8 +76,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public  void setDataToview(int currentPos) {
-        questionTV.setText(String.format("Question Attempted : %d/10", questionsAttempted));
+      private void showbottomsheet() {
+           BottomSheetDialog  bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+           View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.scorebottomsheet, (LinearLayout)findViewById(R.id.idLLScore));
+           TextView scoreTV = bottomSheetView.findViewById(R.id.idTVScore);
+           Button restartQuizBtn = bottomSheetView.findViewById(R.id.idbtnRestart);
+           scoreTV.setText("YOur Score is \n" + currentPos + "/10");
+           restartQuizBtn.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   currentPos = random.nextInt(quizModelArrayList.size());
+                   setDataToview(currentPos);
+                   questionsAttempted = 1;
+                   currentScore = 0;
+                   bottomSheetDialog.dismiss();
+
+               }
+           });
+           bottomSheetDialog.setCancelable(false);
+           bottomSheetDialog.setContentView(bottomSheetView);
+           bottomSheetDialog.show();
+       }
+
+    private void setDataToview(int currentPos) {
+        questionNumberTV.setText("Question Attempted : " + questionsAttempted + "/10");
+        if(questionsAttempted == 10) {
+            showbottomsheet();
+        }
+        else
 
         questionTV.setText(quizModelArrayList.get(this.currentPos).getQuestion());
         option1Btn.setText(quizModelArrayList.get(this.currentPos).getOption1());
@@ -82,12 +114,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getQuizQuestion(ArrayList<QuizModel> quizModelArrayList) {
-        quizModelArrayList.add(new QuizModel("Which of the following is not a programming language ?", "python",  "c/c++", "java","HTML","HTML"));
-        quizModelArrayList.add(new QuizModel("Which of the following are types of computer hardware? Select all that apply. ? ","python",  "c/c++", "java","HTML","HTML"));
-        quizModelArrayList.add(new QuizModel("What is software? ", "Clothing designed to be worn by computer users",  "Flexible parts of a computer case", "Any part of the computer that has a physical structure","Instructions that tell the hardware what to do","Instructions that tell the hardware what to do"));
-        quizModelArrayList.add(new QuizModel("The computer’s main circuit board is called a ________. ?", "hard drive",  "monitor", "cpu","Motherboard","Motherboard"));
-
-
-
+        quizModelArrayList.add(new QuizModel("What is an activity in Android?","Activity performs the actions on the screen",  "Manage the Application content", "Screen UI","None of the above", "Activity performs the actions on the screen"));
+        quizModelArrayList.add(new QuizModel("What are the layouts availabe in Android?", "Linear Layout",  "Table Layout", "Relative Layout","All of the above","All of the above"));
+        quizModelArrayList.add(new QuizModel("What is the lifecycle of broadcast receivers in android?", "send intent()",  "on Receive()", "implicit Broadcast()", "sendBroadcast(), sendOrderBroadcast(), and sendStickyBroadcast","on Receive()"));
+        quizModelArrayList.add(new QuizModel("How to get current location in android?", "Using GPRS", "Using locaiton provider", "A & B","SQlite","A & B"));
+        quizModelArrayList.add(new QuizModel("In which technique, we can refresh the dynamic content in android?", "Java",  "Ajax", "Android","None of the above","Ajax"));
+        quizModelArrayList.add(new QuizModel("What is JSON exception in android?", "JSON Exception",  "Json Not found exception", "input not found exception","None of the above","JSON Exception"));
+        quizModelArrayList.add(new QuizModel("What is a thread in Android", "Same as services",  "Background activity", "Broadcast Receiver","independent dis-patchable unit is called Thread","independent dis-patchable unit is called Thread"));
+        quizModelArrayList.add(new QuizModel("Which component is NOT a part of Android Architecture?", "Android Framework",  "libraries", "Linux Kernal","Android Document","Android Document"));
+        quizModelArrayList.add(new QuizModel("What is APTT?", "Android Asset Processing Tool",  "Android Asset Providing Tool", "Android Asset Packing Tool","Android Asset Packing Technique","Android Asset Packing Tool"));
+        quizModelArrayList.add(new QuizModel("Adb stands for?", "Android Drive Bridge",  "Android Debug Bridge", "Android Destroy Bridge","Android Destroy Bridge","Android Drive Bridge"));
+        //we can add even more questions. Only 10 question will be randomly selected for the qiuiz everything.
     }
 }
