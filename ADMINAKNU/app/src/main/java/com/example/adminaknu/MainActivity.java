83dab68fa9e08170edabe79191ed1a1ee7a2a37d
Lokesh <rocklinglokesh@gmail.com>
2021-10-16 01:07:14 +0530

    startActivityForResult error

--- ADMINAKNU/app/src/main/java/com/example/adminaknu/MainActivity.java
+++ ADMINAKNU/app/src/main/java/com/example/adminaknu/MainActivity.java
@@ -2,19 +2,35 @@ package com.example.adminaknu;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.bluetooth.BluetoothHidDeviceAppSdpSettings;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class  MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    CardView uploadNotice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uploadNotice = findViewById(R.id.addNotice);
+        uploadNotice.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.addNotice:
+                Intent intent = new Intent(MainActivity.this,uploadNotice.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
 
 
